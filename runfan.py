f7e921b28c836dee4bb725351bb5538f3b7de28d
Bassam Husain <bassam.husain@gmail.com>
2023-07-28 01:40:24 -0400

    Adjusted for new physical setup. Ran linter as well.

--- runfan.py
+++ runfan.py
@@ -5,36 +5,39 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-checkInterval = 30 # How often to poll temp
-pin = 24 # The pin ID, edit here to change it
-ledPin = 21 # Pin to turn on LED
-maxTMP = 65 # The high temperature in Celsius which will trigger the fan on
-minTMP = 45 # The low temperature in Celsius which will trigger fan off
+checkInterval = 30  # How often to poll temp
+fanPin = 16  # The pin ID, edit here to change it
+ledPin = 21  # Pin to turn on LED
+maxTMP = 65  # The high temperature in Celsius which will trigger the fan on
+minTMP = 45  # The low temperature in Celsius which will trigger fan off
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
-GPIO.setup(ledPin, GPIO.OUT) # set ledPin as an output
+GPIO.setup(fanPin, GPIO.OUT)
+GPIO.setup(ledPin, GPIO.OUT)  # set ledPin as an output
 GPIO.setwarnings(False)
-    
+
+
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
-    temp =(res.replace("temp=","").replace("'C\n",""))
-    #print(temp)
+    temp = res.replace("temp=", "").replace("'C\n", "")
+    # print(temp)
     return temp
+
 
 def getTEMP():
     CPU_temp = float(getCPUtemperature())
-    if CPU_temp>maxTMP:
-        GPIO.output(pin, True)
+    if CPU_temp > maxTMP:
+        GPIO.output(fanPin, True)
         GPIO.output(ledPin, GPIO.HIGH)
-    elif CPU_temp<minTMP:
-        GPIO.output(pin, False)
+    elif CPU_temp < minTMP:
+        GPIO.output(fanPin, False)
         GPIO.output(ledPin, GPIO.LOW)
-    return()
+    return ()
+
 
 try:
     while True:
         getTEMP()
         time.sleep(checkInterval)
-except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
-    GPIO.cleanup() # resets all GPIO ports used by this program
+except KeyboardInterrupt:  # trap a CTRL+C keyboard interrupt
+    GPIO.cleanup()  # resets all GPIO ports used by this program
